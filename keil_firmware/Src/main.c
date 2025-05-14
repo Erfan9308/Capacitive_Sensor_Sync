2bf3b6e7ce5708e7f28801f4de68ab7ca1eb7cce
Erfan9308 <taheri.erfan90@gmail.com>
2025-05-14 14:36:45 +0200

    Updating main.c while loop transmission

--- keil_firmware/Src/main.c
+++ keil_firmware/Src/main.c
@@ -2,17 +2,11 @@
 /**
   ******************************************************************************
   * @file           : main.c
-  * @brief          : Main program body
+  * @brief          : Main program body for coordinated sensor transmission (4 sensors)
   ******************************************************************************
   * @attention
   *
-  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
+  * This software component is licensed under BSD 3-Clause license.
   *
   ******************************************************************************
   */
@@ -22,24 +16,9 @@
 #include "main.h"
 #include "string.h"
 #include "stdio.h"
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
-
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
+#define BASE_DELAY_MS 10  // Base delay (in ms) for staggered transmission
 
 /* Private variables ---------------------------------------------------------*/
 ADC_HandleTypeDef hadc1;
@@ -53,23 +32,24 @@ DMA_HandleTypeDef hdma_usart1_rx;
 DMA_HandleTypeDef hdma_usart1_tx;
 
 /* USER CODE BEGIN PV */
-uint32_t adcVal[2000]={0};
-uint32_t adcVal2[6000]={0};
-int Ng=243, Ns=256, r=2;
-int k=0,i=0,l=0,j=0,m=0,c=0,p=0,a=0,b=0,d=0;
-uint32_t V1=0,V2=0,V3=0,V4=0;
-uint32_t SRO=0,SLO=0;
-long int Cplatedec1=0,cnt=0;
+// **** Unique parameters per sensor ****
+// Set a unique sensorID for each sensor.
+// For Sensor 1: 0x01, Sensor 2: 0x02, Sensor 3: 0x03, Sensor 4: 0x04.
+uint8_t sensorID = 0x01;  // <<< CHANGE THIS for each sensor accordingly
 
+uint8_t readyBuffer[2];  // Buffer for the "ready" signal from the receiver
+uint8_t xbeeFrame[2];    // Buffer for the XBee API frame
+
+uint32_t adcVal[2000] = {0};
+int Ng = 243, Ns = 256, r = 2;
+int i = 0, l = 0, j = 0, m = 0;
+uint32_t V1 = 0, V2 = 0, V3 = 0, V4 = 0;
+uint32_t SRO = 0, SLO = 0;
+long int Cplatedec1 = 0;
 char ch[7];
 
-
-
-//85nA current
-//const long long int KO=14316339200;
-
-//**12pF**//
-const long long int KO =6352825600;
+// Calibration constant (for a 12pF capacitor configuration)
+const long long int KO = 6352825600;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -80,11 +60,10 @@ static void MX_ADC1_Init(void);
 static void MX_TIM1_Init(void);
 static void MX_TIM2_Init(void);
 static void MX_USART1_UART_Init(void);
-/* USER CODE BEGIN PFP */
 
-/* USER CODE END PFP */
+void processADCValues(void);
+void constructXbeeFrame(uint16_t data);
 
-/* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
 /* USER CODE END 0 */
@@ -95,82 +74,58 @@ static void MX_USART1_UART_Init(void);
   */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-  
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
   SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_DMA_Init();
   MX_ADC1_Init();
   MX_TIM1_Init();
   MX_TIM2_Init();
   MX_USART1_UART_Init();
-  /* USER CODE BEGIN 2 */
-	HAL_ADC_Start_DMA(&hadc1,adcVal,2000);
-	HAL_TIMEx_PWMN_Start(&htim1,TIM_CHANNEL_3);
-	HAL_TIM_Base_Start(&htim2);
-  /* USER CODE END 2 */
- 
- 
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+  HAL_ADC_Start_DMA(&hadc1, adcVal, 1250);
+  HAL_TIMEx_PWMN_Start(&htim1, TIM_CHANNEL_3);
+
   while (1)
   {
-//		HAL_Delay(300);
-		
-		//HAL_UART_Transmit_DMA(&huart1, (uint8_t *)ch, sizeof(ch));
-//    HAL_UART_Transmit(&huart1, (uint8_t *)ch, sizeof(ch), HAL_MAX_DELAY);
-//		HAL_GPIO_TogglePin(LD3_GPIO_Port, LD3_Pin);
+    // Wait for the global "ready" signal from the receiver.
+    if (HAL_UART_Receive(&huart1, readyBuffer, sizeof(readyBuffer), HAL_MAX_DELAY) == HAL_OK)
+    {
+      if (readyBuffer[0] == 0xA0)
+      {
+        // Wait a sensor-specific delay to avoid transmission collisions.
+        HAL_Delay(sensorID * BASE_DELAY_MS);
+        
+        // Process ADC values (synchronized measurement)
+        processADCValues();
+        
+        // Construct the transmission frame (includes sensorID)
+        constructXbeeFrame((uint16_t)Cplatedec1);
+        
+        // Transmit the frame via UART DMA.
+        HAL_UART_Transmit_DMA(&huart1, xbeeFrame, sizeof(xbeeFrame));
+      }
+    }
   }
-	
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
-  /* USER CODE END 3 */
 }
 
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+/* System Clock and Peripheral Initialization functions */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
   RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
-  /** Configure LSE Drive Capability 
-  */
   HAL_PWR_EnableBkUpAccess();
   __HAL_RCC_LSEDRIVE_CONFIG(RCC_LSEDRIVE_LOW);
-  /** Configure the main internal regulator output voltage 
-  */
+
   if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
   {
     Error_Handler();
   }
-  /** Initializes the CPU, AHB and APB busses clocks 
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSE|RCC_OSCILLATORTYPE_MSI;
+
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSE | RCC_OSCILLATORTYPE_MSI;
   RCC_OscInitStruct.LSEState = RCC_LSE_ON;
   RCC_OscInitStruct.MSIState = RCC_MSI_ON;
   RCC_OscInitStruct.MSICalibrationValue = 0;
@@ -185,50 +140,31 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
-  /** Initializes the CPU, AHB and APB busses clocks 
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
+                                RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-
   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
   {
     Error_Handler();
   }
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1|RCC_PERIPHCLK_ADC;
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1 | RCC_PERIPHCLK_ADC;
   PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK2;
   if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
   {
     Error_Handler();
   }
-  /** Enable MSI Auto calibration 
-  */
   HAL_RCCEx_EnableMSIPLLMode();
 }
 
-/**
-  * @brief ADC1 Initialization Function
-  * @param None
-  * @retval None
-  */
 static void MX_ADC1_Init(void)
 {
-
-  /* USER CODE BEGIN ADC1_Init 0 */
-
-  /* USER CODE END ADC1_Init 0 */
-
   ADC_MultiModeTypeDef multimode = {0};
   ADC_ChannelConfTypeDef sConfig = {0};
 
-  /* USER CODE BEGIN ADC1_Init 1 */
-
-  /* USER CODE END ADC1_Init 1 */
-  /** Common config 
-  */
   hadc1.Instance = ADC1;
   hadc1.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV1;
   hadc1.Init.Resolution = ADC_RESOLUTION_12B;
@@ -249,17 +185,13 @@ static void MX_ADC1_Init(void)
   {
     Error_Handler();
   }
-  /** Configure the ADC multi-mode 
-  */
-  multimode.DMAAccessMode = ADC_DMAACCESSMODE_DISABLED;
-	multimode.TwoSamplingDelay = ADC_TWOSAMPLINGDELAY_1CYCLE;
-	
+
+  multimode.Mode = ADC_MODE_INDEPENDENT;
   if (HAL_ADCEx_MultiModeConfigChannel(&hadc1, &multimode) != HAL_OK)
   {
     Error_Handler();
   }
-  /** Configure Regular Channel 
-  */
+
   sConfig.Channel = ADC_CHANNEL_5;
   sConfig.Rank = ADC_REGULAR_RANK_1;
   sConfig.SamplingTime = ADC_SAMPLETIME_2CYCLES_5;
@@ -270,34 +202,19 @@ static void MX_ADC1_Init(void)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN ADC1_Init 2 */
-	HAL_ADCEx_Calibration_Start(&hadc1,ADC_SINGLE_ENDED);
-  /* USER CODE END ADC1_Init 2 */
-
+  
+  HAL_ADCEx_Calibration_Start(&hadc1, ADC_SINGLE_ENDED);
 }
 
-/**
-  * @brief TIM1 Initialization Function
-  * @param None
-  * @retval None
-  */
 static void MX_TIM1_Init(void)
 {
-
-  /* USER CODE BEGIN TIM1_Init 0 */
-
-  /* USER CODE END TIM1_Init 0 */
-
   TIM_ClockConfigTypeDef sClockSourceConfig = {0};
   TIM_MasterConfigTypeDef sMasterConfig = {0};
   TIM_OC_InitTypeDef sConfigOC = {0};
   TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
 
-  /* USER CODE BEGIN TIM1_Init 1 */
-
-  /* USER CODE END TIM1_Init 1 */
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 1;
+  htim1.Init.Prescaler = 0;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim1.Init.Period = 39999;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
@@ -307,15 +224,18 @@ static void MX_TIM1_Init(void)
   {
     Error_Handler();
   }
+  
   sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
   if (HAL_TIM_ConfigClockSource(&htim1, &sClockSourceConfig) != HAL_OK)
   {
     Error_Handler();
   }
+  
   if (HAL_TIM_PWM_Init(&htim1) != HAL_OK)
   {
     Error_Handler();
   }
+  
   sMasterConfig.MasterOutputTrigger = TIM_TRGO_UPDATE;
   sMasterConfig.MasterOutputTrigger2 = TIM_TRGO2_RESET;
   sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
@@ -323,6 +243,7 @@ static void MX_TIM1_Init(void)
   {
     Error_Handler();
   }
+  
   sConfigOC.OCMode = TIM_OCMODE_PWM1;
   sConfigOC.Pulse = 19999;
   sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
@@ -334,6 +255,7 @@ static void MX_TIM1_Init(void)
   {
     Error_Handler();
   }
+  
   sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
   sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
   sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
@@ -349,32 +271,15 @@ static void MX_TIM1_Init(void)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN TIM1_Init 2 */
-
-  /* USER CODE END TIM1_Init 2 */
   HAL_TIM_MspPostInit(&htim1);
-
 }
 
-/**
-  * @brief TIM2 Initialization Function
-  * @param None
-  * @retval None
-  */
 static void MX_TIM2_Init(void)
 {
-
-  /* USER CODE BEGIN TIM2_Init 0 */
-
-  /* USER CODE END TIM2_Init 0 */
-
   TIM_ClockConfigTypeDef sClockSourceConfig = {0};
   TIM_SlaveConfigTypeDef sSlaveConfig = {0};
   TIM_MasterConfigTypeDef sMasterConfig = {0};
 
-  /* USER CODE BEGIN TIM2_Init 1 */
-
-  /* USER CODE END TIM2_Init 1 */
   htim2.Instance = TIM2;
   htim2.Init.Prescaler = 0;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
@@ -385,44 +290,30 @@ static void MX_TIM2_Init(void)
   {
     Error_Handler();
   }
+  
   sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
   if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
   {
     Error_Handler();
   }
-  sSlaveConfig.SlaveMode = TIM_SLAVEMODE_TRIGGER;
+  
+  sSlaveConfig.SlaveMode = TIM_SLAVEMODE_COMBINED_RESETTRIGGER;
   sSlaveConfig.InputTrigger = TIM_TS_ITR0;
   if (HAL_TIM_SlaveConfigSynchro(&htim2, &sSlaveConfig) != HAL_OK)
   {
     Error_Handler();
   }
+  
   sMasterConfig.MasterOutputTrigger = TIM_TRGO_UPDATE;
   sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
   if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN TIM2_Init 2 */
-
-  /* USER CODE END TIM2_Init 2 */
-
 }
 
-/**
-  * @brief USART1 Initialization Function
-  * @param None
-  * @retval None
-  */
 static void MX_USART1_UART_Init(void)
 {
-
-  /* USER CODE BEGIN USART1_Init 0 */
-
-  /* USER CODE END USART1_Init 0 */
-
-  /* USER CODE BEGIN USART1_Init 1 */
-
-  /* USER CODE END USART1_Init 1 */
   huart1.Instance = USART1;
   huart1.Init.BaudRate = 115200;
   huart1.Init.WordLength = UART_WORDLENGTH_8B;
@@ -437,153 +328,103 @@ static void MX_USART1_UART_Init(void)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN USART1_Init 2 */
-
-  /* USER CODE END USART1_Init 2 */
-
 }
 
-/** 
-  * Enable DMA controller clock
-  */
-static void MX_DMA_Init(void) 
+static void MX_DMA_Init(void)
 {
-
-  /* DMA controller clock enable */
   __HAL_RCC_DMA1_CLK_ENABLE();
   __HAL_RCC_DMA2_CLK_ENABLE();
 
-  /* DMA interrupt init */
-  /* DMA1_Channel1_IRQn interrupt configuration */
   HAL_NVIC_SetPriority(DMA1_Channel1_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(DMA1_Channel1_IRQn);
-//  /* DMA2_Channel6_IRQn interrupt configuration */
-//  HAL_NVIC_SetPriority(DMA2_Channel6_IRQn, 0, 0);
-//  HAL_NVIC_EnableIRQ(DMA2_Channel6_IRQn);
-//  /* DMA2_Channel7_IRQn interrupt configuration */
-//  HAL_NVIC_SetPriority(DMA2_Channel7_IRQn, 0, 0);
-//  HAL_NVIC_EnableIRQ(DMA2_Channel7_IRQn);
-
+  
+  HAL_NVIC_SetPriority(DMA2_Channel6_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA2_Channel6_IRQn);
+  
+  HAL_NVIC_SetPriority(DMA2_Channel7_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA2_Channel7_IRQn);
 }
 
-/**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
 
-  /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
-  /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : LD3_Pin */
   GPIO_InitStruct.Pin = LD3_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(LD3_GPIO_Port, &GPIO_InitStruct);
-
 }
 
 /* USER CODE BEGIN 4 */
+void processADCValues(void)
+{
+    V1 = 0;
+    for (i = Ng; i < Ng + Ns; i++) {
+        V1 += adcVal[i];
+    }
+
+    V2 = 0;
+    for (l = Ng + Ns + r; l < Ng + 2 * Ns + r; l++) {
+        V2 += adcVal[l];
+    }
+    SLO = (V1 - V2) / 16;
+    
+    V3 = 0;
+    for (j = 3 * Ng + 2 * Ns + r; j < 3 * Ng + 3 * Ns + r; j++) {
+        V3 += adcVal[j];
+    }
+
+    V4 = 0;
+    for (m = 3 * Ng + 3 * Ns + 2 * r; m < 3 * Ng + 4 * Ns + 2 * r; m++) {
+        V4 += adcVal[m];
+    }
+    SRO = (V4 - V3) / 16;
+    Cplatedec1 = KO / (33 * (SLO + SRO));
+}
+
+void constructXbeeFrame(uint16_t adcValue)
+{
+    xbeeFrame[0] = (sensorID << 4) | ((adcValue >> 8) & 0x0F);
+    xbeeFrame[1] = adcValue & 0xFF;
+}
 
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc)
-{		
-V1=0;
-	for(i=Ng;i<Ng+Ns;i++){
-		V1+=adcVal[i];
-	}
-	
-	V2=0;
-	for(l=Ng+Ns+r;l<Ng+2*Ns+r;l++){
-		V2+=adcVal[l];	
-	}
-	
-	SLO=(V1-V2)/16;
-	V3=0;
-	for(j=3*Ng+2*Ns+r;j<3*Ng+3*Ns+r;j++){
-		V3+=adcVal[j];	
-	}
-	
-	V4=0;
-	for(m=3*Ng+3*Ns+2*r;m<3*Ng+4*Ns+2*r;m++){
-		V4+=adcVal[m];	
-	}
-	
-	SRO=(V4-V3)/16;
-//***********************************************		
-	
-	Cplatedec1=KO/(33*(SLO+SRO));
-	
-//	Cplatedec1=(SLO+SRO)/2;
-	
-	
-	sprintf(ch,"%ld\r\n",Cplatedec1);
-	//sprintf(ch,"a \r\n");
-	HAL_UART_Transmit(&huart1,(uint8_t *) &ch,strlen(ch), 1);
-//	a++;
-//	if(a==8000){
-//		HAL_GPIO_TogglePin(LD3_GPIO_Port, LD3_Pin);
-//		//HAL_UART_Transmit(&huart1,(unsigned char *) &ch, 1,100);
-//		a=0;
-//	}
-
-//***********debug*****************************************	
-//	HAL_ADC_Stop_DMA(&hadc1);
-//	HAL_TIMEx_PWMN_Stop(&htim1, TIM_CHANNEL_3);
-//	HAL_TIM_Base_Stop(&htim2);
-	
-//	HAL_UART_Transmit(&huart2,(uint8_t *) "start\r\n",strlen("start\r\n"), HAL_MAX_DELAY);
-//	for (a=0;a<6000;a++){
-//		sprintf(ch,"%d\r\n", adcVal[a]);
-//		HAL_UART_Transmit(&huart2,(uint8_t *) &ch,strlen(ch), HAL_MAX_DELAY);
-//	}
-//	HAL_ADC_Start_DMA(&hadc1,adcVal,6000);
-//	HAL_TIMEx_PWMN_Start(&htim1, TIM_CHANNEL_3);
-//	HAL_TIM_Base_Start(&htim2);
-
+{
+    processADCValues();
+    
+    sprintf(ch, "%ld\r\n", Cplatedec1);
+    HAL_UART_Transmit_DMA(&huart1, (uint8_t *)ch, sizeof(ch));
+    
+    static int a = 0;
+    a++;
+    if (a == 8000)
+    {
+        HAL_GPIO_TogglePin(LD3_GPIO_Port, LD3_Pin);
+        a = 0;
+    }
 }
 
 void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart)
 {
 }
-
 /* USER CODE END 4 */
 
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-
-  /* USER CODE END Error_Handler_Debug */
+  while(1)
+  {
+  }
 }
 
-#ifdef  USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+#ifdef USE_FULL_ASSERT
 void assert_failed(char *file, uint32_t line)
-{ 
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+{
 }
 #endif /* USE_FULL_ASSERT */
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
