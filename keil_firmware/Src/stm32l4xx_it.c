6e2034e23462b895ceb42df454a8e79ef58f8346
Erfan Taheri <168378798+Erfan9308@users.noreply.github.com>
2025-11-18 00:45:25 +0100

    Clean up unused DMA and USART interrupt handlers
    Removed unused interrupt handlers and external variable declarations for DMA and UART.

--- keil_firmware/Src/stm32l4xx_it.c
+++ keil_firmware/Src/stm32l4xx_it.c
@@ -56,10 +56,7 @@
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
-extern DMA_HandleTypeDef hdma_adc1;
-extern DMA_HandleTypeDef hdma_usart1_rx;
-extern DMA_HandleTypeDef hdma_usart1_tx;
-extern UART_HandleTypeDef huart1;
+
 /* USER CODE BEGIN EV */
 
 /* USER CODE END EV */
@@ -203,58 +200,20 @@ void SysTick_Handler(void)
 /**
   * @brief This function handles DMA1 channel1 global interrupt.
   */
-void DMA1_Channel1_IRQHandler(void)
-{
-  /* USER CODE BEGIN DMA1_Channel1_IRQn 0 */
 
-  /* USER CODE END DMA1_Channel1_IRQn 0 */
-  HAL_DMA_IRQHandler(&hdma_adc1);
-  /* USER CODE BEGIN DMA1_Channel1_IRQn 1 */
-
-  /* USER CODE END DMA1_Channel1_IRQn 1 */
-}
 
 /**
   * @brief This function handles USART1 global interrupt.
   */
-void USART1_IRQHandler(void)
-{
-  /* USER CODE BEGIN USART1_IRQn 0 */
 
-  /* USER CODE END USART1_IRQn 0 */
-  HAL_UART_IRQHandler(&huart1);
-  /* USER CODE BEGIN USART1_IRQn 1 */
-
-  /* USER CODE END USART1_IRQn 1 */
-}
 
 /**
   * @brief This function handles DMA2 channel6 global interrupt.
   */
-void DMA2_Channel6_IRQHandler(void)
-{
-  /* USER CODE BEGIN DMA2_Channel6_IRQn 0 */
-
-  /* USER CODE END DMA2_Channel6_IRQn 0 */
-  HAL_DMA_IRQHandler(&hdma_usart1_tx);
-  /* USER CODE BEGIN DMA2_Channel6_IRQn 1 */
-
-  /* USER CODE END DMA2_Channel6_IRQn 1 */
-}
 
 /**
   * @brief This function handles DMA2 channel7 global interrupt.
   */
-void DMA2_Channel7_IRQHandler(void)
-{
-  /* USER CODE BEGIN DMA2_Channel7_IRQn 0 */
-
-  /* USER CODE END DMA2_Channel7_IRQn 0 */
-  HAL_DMA_IRQHandler(&hdma_usart1_rx);
-  /* USER CODE BEGIN DMA2_Channel7_IRQn 1 */
-
-  /* USER CODE END DMA2_Channel7_IRQn 1 */
-}
 
 /* USER CODE BEGIN 1 */
 
